071562bc6299f110d187dc1e1bd7ab7bf2a454b5
Vinh Le <lethanhvinh95@gmail.com>
2025-01-18 22:01:07 +0200

    add new domain to cors

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def log_requests(request: Request, call_next):
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://stock-agent.netlify.app"], 
+    allow_origins=["http://localhost:3000", "https://stonkie.netlify.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
