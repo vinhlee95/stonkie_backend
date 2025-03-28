ffb1c0609f582380c1289c268a0b1687ce916f65
Vinh Le <lethanhvinh95@gmail.com>
2025-03-28 07:49:11 +0200

    update origin

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def log_requests(request: Request, call_next):
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://stonkie.netlify.app"], 
+    allow_origins=["http://localhost:3000", "https://stonkie.netlify.app", "https://stonkie.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
