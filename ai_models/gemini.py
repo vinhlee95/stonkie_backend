476a68c8d8bf10a5bf6ae84a72794447264d9f81
Vinh Le <lethanhvinh95@gmail.com>
2025-05-10 14:33:23 +0300

    use 2.0 flash model for better output speed

--- ai_models/gemini.py
+++ ai_models/gemini.py
@@ -12,7 +12,8 @@ class GeminiModel:
         """Initialize the Gemini agent with API key configuration"""
         
         self.api_key = os.getenv("GEMINI_API_KEY")
-        MODEL_NAME = "gemini-2.5-flash-preview-04-17"
+        # MODEL_NAME = "gemini-2.5-flash-preview-04-17"
+        MODEL_NAME = "gemini-2.0-flash"
 
         if not self.api_key:
             raise ValueError("GEMINI_API_KEY environment variable not found")
