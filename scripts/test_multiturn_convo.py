deb3170862a8f83430db479d7f6b297e89690e34
Vinh Le <lethanhvinh95@gmail.com>
2025-07-10 17:05:36 +0700

    test chat convo

---
+++ scripts/test_multiturn_convo.py
@@ -0,0 +1,21 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+chat = client.chats.create(model="gemini-2.5-flash-lite-preview-06-17")
+
+response = chat.send_message_stream("How does asset to equity ratio tell about a business? Give the answer in 10 words.")
+print("First response:")
+for chunk in response:
+  print(chunk.text, end="")
+
+response = chat.send_message_stream("How is that metric calculated? Give the answer in as much details as possible.")
+print("Second response:")
+for chunk in response:
+  print(chunk.text, end="")
+
+# for message in chat.get_history():
+#     print(f'role - {message.role}', end=": ")
+#     print(message.parts[0].text)
