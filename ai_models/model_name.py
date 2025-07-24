aaa4e72bc390673c1110276b8df69ea94ea5aeec
Vinh Le <lethanhvinh95@gmail.com>
2025-07-24 09:21:43 +0700

    use non-beta 2.5 flash lite

--- ai_models/model_name.py
+++ ai_models/model_name.py
@@ -2,6 +2,6 @@ from enum import StrEnum
 
 class ModelName(StrEnum):
     # https://cloud.google.com/vertex-ai/generative-ai/docs/models/gemini/2-5-pro
-    Gemini25FlashLite = "gemini-2.5-flash-lite-preview-06-17"
+    Gemini25FlashLite = "gemini-2.5-flash-lite"
     GeminiFlash = "gemini-2.5-flash"
     GemimiPro = "gemini-2.5-pro"
